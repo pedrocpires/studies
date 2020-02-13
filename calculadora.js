432512ee58c3817eceb6691f706838349e196af0
Pedro Pires <pedro.cpires@hotmail.com>
2020-02-13 20:10:48 -0300

    Criação do calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+console.clear();
+const tituloPrograma = "CALCULADORA BÀSICA"
+console.log("=".repeat(tituloPrograma.length));
+console.log(tituloPrograma);
+console.log("=".repeat(tituloPrograma.length));
+// var nome = raw_input("Nome:");
+// let nome = readline();
+var name = window.prompt("Msg 1", "Msg2");
